fc424e73027cdce5ac003865c24c3afe58a2c457
Miguel Alpendre <miguel.alpendre@olx.com>
2018-03-20 18:16:27 +0000

    Guzzle client

---
+++ post.php
@@ -0,0 +1,34 @@
+<?php
+require 'vendor/autoload.php';
+
+$client = new \GuzzleHttp\Client();
+$debug = true;
+
+$domain = 'https://teste.stg.eu-west-1.mercury.olx.org/';
+$path = 'sitehooks/v1/messages';
+
+$apiKey = 'za7bzeRMuEqvf4c95T9tfh7h8JkmLUwq';
+
+$uuid = '123456';
+$message = 'Some Message from Guzzle.';
+
+$res = $client->request('POST', sprintf("%s%s", $domain, $path), [
+        'headers' => [
+            //'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'x-api-key' => $apiKey
+        ],
+        \GuzzleHttp\RequestOptions::JSON => [
+            'uuid' => $uuid,
+            'message' => $message
+        ],
+        'debug' => $debug,
+        'protocols' => ['https']
+        ]
+);
+
+echo "Status Code: ".$res->getStatusCode()."\n";
+echo "Response: ".$res->getBody()."\n";
+
+//echo "Done.";
+
